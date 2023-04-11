4dc935ff04279208d54e28d5b5c4c85996dbba2b
MuchamadAndriyanto <mandriyanto9@gmail.com>
2023-04-12 00:59:35 +0700

    Update Home Fragment

--- app/src/main/java/com/example/latihansharedpreference/HomeFragment.kt
+++ app/src/main/java/com/example/latihansharedpreference/HomeFragment.kt
@@ -1,24 +1,40 @@
 package com.example.latihansharedpreference
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
-
+import androidx.navigation.Navigation
+import com.example.latihansharedpreference.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-
+    lateinit var binding : FragmentHomeBinding
+    lateinit var dataPref : SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataPref= requireContext().getSharedPreferences("dataregistrasi", Context.MODE_PRIVATE)
+        binding.tvHello.text = "Hello,  " + dataPref.getString("name", "null")
 
+        binding.btnLogout.setOnClickListener {
+            val dataPref = dataPref.edit()
+            dataPref.clear()
+            dataPref.apply()
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment2_to_loginFragment)
+
+        }
+
+    }
 }
