ba2aba14ca69a5d2dc95110644512a4ffb16ffeb
LordKajoc <rizkinuriman@upi.edu>
2023-04-12 14:51:36 +0700

    Update NavHost and Aksi SplashScreen

--- app/src/main/java/com/example/latihansharedpreference/LoginFragment.kt
+++ app/src/main/java/com/example/latihansharedpreference/LoginFragment.kt
@@ -13,8 +13,8 @@ import com.example.latihansharedpreference.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
-    lateinit var binding : FragmentLoginBinding
-    lateinit var dataPref : SharedPreferences
+    lateinit var binding: FragmentLoginBinding
+    lateinit var dataPref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +24,6 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
 
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,24 +31,26 @@ class LoginFragment : Fragment() {
 
         dataPref = requireContext().getSharedPreferences("dataregistrasi", Context.MODE_PRIVATE)
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             var getUsername = dataPref.getString("username", "")
             var getPassword = dataPref.getString("password", "")
 
             var username = binding.logUsername.text.toString()
             var password = binding.logPassword.text.toString()
 
-            if (username == getUsername && password.equals(getPassword)){
-                Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_homeFragment2)
+            if (username == getUsername && password.equals(getPassword)) {
+                Navigation.findNavController(requireView())
+                    .navigate(R.id.action_loginFragment_to_homeFragment2)
 
-            }else{
-                Toast.makeText(context, "Username atau password anda salah !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Username atau password anda salah !", Toast.LENGTH_SHORT)
+                    .show()
             }
-
         }
 
-        binding.textReg.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment)
+        binding.textReg.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_loginFragment_to_registerFragment)
         }
     }
 
--- app/src/main/java/com/example/latihansharedpreference/SplashFragment.kt
+++ app/src/main/java/com/example/latihansharedpreference/SplashFragment.kt
@@ -1,20 +1,39 @@
 package com.example.latihansharedpreference
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.latihansharedpreference.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
 
+    lateinit var binding: FragmentSplashBinding
+    lateinit var sharedSplash: SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        Handler(Looper.myLooper()!!).postDelayed({
+
+            sharedSplash =
+                requireContext().getSharedPreferences("dataregistrasi", Context.MODE_PRIVATE)
+            if (sharedSplash.getString("nama", "").equals("")) {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment2)
+            } else if (sharedSplash.getString("nama", "")!!.isNotEmpty()) {
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+        }, 3000)
+        return binding.root
     }
 
 
